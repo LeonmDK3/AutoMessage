e8d357fd9a11456f3c65fd250777e360a93eff3e
SnapDragon-cmd <64895070+SnapDragon-cmd@users.noreply.github.com>
2020-05-22 01:45:11 +0300

    Add files via upload

---
+++ AutoMessage/config.lua
@@ -0,0 +1,46 @@
+config={} 
+config.time = 3   -- Kaç dakikada bir mesaj atsın? (Şuanda 3 dakikada bir atar.)
+config.name = '^1DenemeRP Otomatik Mesaj: ' -- Otomatik mesajın başındaki yazı. (Sunucu isminiz olarak ayarlayabilirsiniz.)
+config.enabled = true
+config.messages = {
+    'Test1',
+    'Test2',
+    'Test3',
+    'Test4',
+    'Test5',
+    'Test6',
+    'Test7',
+    'Test8',
+    'Test9',
+    'Test10',
+}
+config.tfmsg = {
+[true] = 'Activated',
+[false] = 'Deactivated',
+}
+
+--  @@@@@@@@  @  @       @  @@@@@@@@@  @         @                                                                                
+--  @         @  @       @  @          @@       @@                                                                                
+--  @         @   @     @   @          @@       @@                                                                                                                                                                          
+--  @         @   @     @   @          @ @     @ @                                                        
+--  @         @   @     @   @          @ @     @ @                                                        
+--  @         @    @   @    @@@@@@@@   @  @   @  @                                                        
+--  @@@@@@@   @    @   @    @          @  @   @  @                                                        
+--  @         @    @   @    @          @   @ @   @                                                        
+--  @         @     @ @     @          @   @ @   @                                                        
+--  @         @     @ @     @          @   @ @   @                                                        
+--  @         @      @      @          @    @    @                                                        
+--  @         @      @      @@@@@@@@@  @    @    @                                                        
+                                                                                       
+-- @@@@@@@@@ @       @  @@@@@@@    @       @                                                                   
+--     @     @       @  @      @   @      @                                                                                      
+--     @     @       @  @       @  @     @                                                                                        
+--     @     @       @  @       @  @    @                                                                                         
+--     @     @       @  @       @  @   @                                                                                          
+--     @     @       @  @      @   @  @@                                                                                          
+--     @     @       @  @@@@@@@    @ @ @                                                                                          
+--     @     @       @  @    @     @@   @                                                                                         
+--     @     @       @  @     @    @     @                                                                                        
+--     @     @       @  @     @    @     @                                                                                        
+--     @      @     @   @      @   @      @                                                                                       
+--     @       @@@@@    @       @  @       @                                                                                      
